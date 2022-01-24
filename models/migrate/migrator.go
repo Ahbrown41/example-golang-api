04729f6169654a168777c9061d8af7ffc9bd456e
Alex Brown <alex@redantelope.com>
2022-01-24 09:43:26 -0500

    Fix attribution

--- models/migrate/migrator.go
+++ models/migrate/migrator.go
@@ -2,9 +2,9 @@
  * (c)  2022 – Example, Inc.
  * All Rights Reserved.
  *
- * NOTICE:  All information contained herein is, and shall at all times remain, the sole and exclusive property of Wawa, Inc.
- * All such information, including the intellectual and technical concepts contained therein, are proprietary to Wawa, Inc. and are protected by copyright law.
- * Dissemination or reproduction of this information is strictly forbidden unless prior written permission is obtained from a duly authorized representative of Wawa, Inc.
+ * NOTICE:  All information contained herein is, and shall at all times remain, the sole and exclusive property ofExample, Inc.
+ * All such information, including the intellectual and technical concepts contained therein, are proprietary toExample, Inc. and are protected by copyright law.
+ * Dissemination or reproduction of this information is strictly forbidden unless prior written permission is obtained from a duly authorized representative ofExample, Inc.
  */
 
 package migrate
--- release.sh
+++ release.sh
@@ -1,10 +1,10 @@
 #
-# (c)  2021 – Wawa, Inc.
+# (c)  2021 –Example, Inc.
 # All Rights Reserved.
 #
-# NOTICE:  All information contained herein is, and shall at all times remain, the sole and exclusive property of Wawa, Inc.
-# All such information, including the intellectual and technical concepts contained therein, are proprietary to Wawa, Inc. and are protected by copyright law.
-# Dissemination or reproduction of this information is strictly forbidden unless prior written permission is obtained from a duly authorized representative of Wawa, Inc.
+# NOTICE:  All information contained herein is, and shall at all times remain, the sole and exclusive property ofExample, Inc.
+# All such information, including the intellectual and technical concepts contained therein, are proprietary toExample, Inc. and are protected by copyright law.
+# Dissemination or reproduction of this information is strictly forbidden unless prior written permission is obtained from a duly authorized representative ofExample, Inc.
 #
 
 export VERSION=`cat ./VERSION | xargs`
